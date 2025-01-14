85bc4eacfa25ad5adad0a8f3120b5ca0fe8ba886
Manohar Kale <manoharkale5368@gmail.com>
2025-01-14 12:09:39 +0530

    create hero section

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ function page() {
   return (
     <main>
       <section className="bg-[#254f1a] min-h-[100vh] grid grid-cols-2">
-        <div className="flex flex-col justify-center ml-20 gap-5">
+        <div className="flex flex-col mt-20 justify-center ml-20 gap-5">
           <p className="text-5xl font-extrabold text-[#d2e823]">
             Everything you are. <br /> In one, simple link in bio.
           </p>
@@ -14,6 +14,10 @@ function page() {
             help you share everything you create, curate and sell from your
             Instagram, TikTok, Twitter, YouTube and other social media profiles.
           </p>
+          <div className="flex gap-2 py-6">
+            <input className="p-3 text-sm rounded-lg focus:outline-green-[#254f1a]" type="text" placeholder="linktr.ee/your-url" />
+            <button className="bg-yellow-300">Claim your Linktree</button>
+          </div>
         </div>
         <div className="flex justify-center items-end">
           <Image src="/hero.png" alt="hero image" width={450} height={450} />
