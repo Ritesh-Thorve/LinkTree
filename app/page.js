4954d47de6ab1c9ee31fc867c4120442a21c5607
Manohar Kale <manoharkale5368@gmail.com>
2025-01-14 11:33:44 +0530

    create hero section

--- app/page.js
+++ app/page.js
@@ -3,9 +3,20 @@ import React from "react";
 function page() {
   return (
     <main>
-      <section className="bg-[#254f1a] min-h-screen">
-
+      <section className="bg-[#254f1a] min-h-[100vh] grid grid-cols-2">
+        <div className="bg-red-300">
+          <p>Everything you are. In one, simple link in bio.</p>
+          <p>
+            Join 50M+ people using Linktree for their link in bio. One link to
+            help you share everything you create, curate and sell from your
+            Instagram, TikTok, Twitter, YouTube and other social media profiles.
+          </p>
+        </div>
+        <div className="bg-slate-50">
+          <h1>chalo</h1>
+        </div>
       </section>
+      <section className="bg-[#a1144c] min-h-[100vh]"></section>
     </main>
   );
 }
