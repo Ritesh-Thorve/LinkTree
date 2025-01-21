9cecc0202616ec3c0c30d7ee85591113cdb6099a
Manohar Kale <manoharkale5368@gmail.com>
2025-01-21 17:21:13 +0530

    created API for link tree generator page

---
+++ app/api/add/route.js
@@ -0,0 +1,14 @@
+import clientPromise from "@/lib/mongodb";
+
+export async function POST(request) {
+  const body = await request.json();
+
+  const client = await clientPromise;
+  const db = client.db("linktree");
+  const collection = db.collection("links");
+  const result = await collection.insertOne(body);
+  return Response.json({
+    status: 200,
+    result: result,
+  });
+}
